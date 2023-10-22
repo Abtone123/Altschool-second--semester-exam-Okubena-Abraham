d070ec14dba337d06853dd4a3f0b2a9937d02b5d
Abtone123 <abrahamokubena@gmail.com>
2023-10-22 13:10:57 +0100

    added a link back to home page on not found page

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import "./index.css";
+import {Link} from "react-router-dom"
 
 const NotFound = () => {
-  return (
+  return <>
     <h1 style={
         {
             textAlign: "center",
@@ -11,7 +12,8 @@ const NotFound = () => {
             color: "#c3f6f6"
         }
     }>This page does not exist</h1>
-  )
+   <Link to="/"><button>Go to home page</button></Link> 
+  </>
 }
 
 export default NotFound;
